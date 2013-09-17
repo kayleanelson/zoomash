7226bfeec60bd4d6ee994d1198adc3f8c1d67326
gs-spruce.it <gs-spruce.it@n17.c10.mtsvc.net>
2013-09-17 10:24:50 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Zoo Mash" />
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+ 
+ 
+ 
+<title>Zoo Mash</title>
+
+</head>
+<body>
+
+
+<header>
+
+<h1>Zoo Mash</h1>
+
+</header>
+
+<section id="instructions">
+ <p>Choose which photo you think is more beautiful:</p>
+</section>
+
+<section id="comparison">
+<div class="image">
+	<img src="http://www.snapshotserengeti.org/images/tutorial-subject/PICT0501.JPG" />
+</div>
+
+<div class="image">
+	<img src="http://www.snapshotserengeti.org/images/tutorial-subject/PICT0501.JPG" />
+</div>
+
+</section>
+
+
+
+</body>
+</html>
