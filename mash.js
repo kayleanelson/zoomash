bdcf18d360e449f4efde09b026770a17d7f9d86b
Kaylea Nelson <Kaylea@Astrid.local>
2013-09-17 23:30:45 -0400

    added topshots page

--- mash.js
+++ mash.js
@@ -22,8 +22,8 @@ $("div.image").click(function(){
 	win_prob_winner = 1/( Math.pow(10, (rank_loser - rank_winner)/400.0)+1.0);
 	win_prob_loser = 1/( Math.pow(10, (rank_winner - rank_loser)/400.0)+1.0);
 
-	rank_loser = rank_loser - kvalue * win_prob_loser;
 	rank_winner = rank_winner + kvalue*(1.0-win_prob_winner);
+	rank_loser = rank_loser - kvalue * win_prob_loser;
 
 	winner_outgoing = {
 		id: winner._id,
@@ -35,11 +35,14 @@ $("div.image").click(function(){
 		rank: rank_loser
 	};
 
+//	console.log(loser_outgoing)
+//	console.log(winner_outgoing)
+
 	$.post("classification", winner_outgoing);
 	$.post("classification", loser_outgoing);
 
 
-	location.reload();
+//	location.reload();
 
 });
 
